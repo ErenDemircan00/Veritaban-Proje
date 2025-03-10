c9fc48a7bbed277dab8204bbfd62c0dca09e5239
Alper Timucin Akyol <akyolalper89@gmail.com>
2025-03-10 22:55:56 +0300

    Güncellemeler yapıldı

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
-class User(db.Model):
+class (db.Model):User
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
