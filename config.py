2ffcd704a6e69d5e5e4be658611291f761a22e58
Eren Demircan <154807775+ErenDemircan00@users.noreply.github.com>
2025-03-12 15:54:21 +0300

    veritabani bağlantısı yapıldı

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://kullanici_adi:sifre@localhost/veritabani_adi'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://kullanici_adi:sifre@localhost/flask_veritabani'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'supergizlisifre'  # JWT için gizli anahtar
+    JWT_SECRET_KEY = 'supergizlisifre'  
