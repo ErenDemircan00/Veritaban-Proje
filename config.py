d68b0ed4e9c0899742c5077c953945b3c6ac6dab
Eren Demircan <154807775+ErenDemircan00@users.noreply.github.com>
2025-03-18 16:14:51 +0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:VeritabaniProje123@localhost/flask_veritabani'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'supergizlisifre'  
