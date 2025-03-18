1f757891b1e3561cca0bdc0f0e0f71f12b9e331c
Eren Demircan <154807775+ErenDemircan00@users.noreply.github.com>
2025-03-18 15:34:59 +0300

    map,lambda,filter,input

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://kullanici_adi:sifre@localhost/flask_veritabani'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:VeritabaniProje123@localhost/flask_veritabani'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'supergizlisifre'  
